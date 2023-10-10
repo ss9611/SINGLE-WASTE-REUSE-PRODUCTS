859d3a6cc12a2425d276992cdab5ef2341ce4beb
SHAIK MAHABOOB ARSHAD <103995389+ss9611@users.noreply.github.com>
2023-10-10 19:40:55 +0530

    Add files via upload

---
+++ indjava.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change slide every 2 seconds (adjust as needed)
+}
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+showSlides();
